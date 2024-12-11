128b7d5b2c67ce95e7aaa98b062b33df4ee7380e
Coding Tiger <coding.tiger.mail@gmail.com>
2024-12-11 12:14:26 +0530

    server cors url changed 2

--- server/index.js
+++ server/index.js
@@ -8,7 +8,12 @@ const app = express();
 dotenv.config();
 connectDB();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
 app.use(express.json());
 app.use("/api", router);
 
